d31194a5acc6512b346e8e1d065325f9b398e7f6
fenggao66 <140604213+fenggao66@users.noreply.github.com>
2023-07-26 18:20:19 +0800

    Update train_test_SVM.py

--- base_detectors/base model/train_test_SVM.py
+++ base_detectors/base model/train_test_SVM.py
@@ -12,16 +12,6 @@ from sklearn import metrics
 # path_attack= "C:/Users/gjh/Desktop/RLMSAD-master/base_detectors/raw_data/SWaT/1111_v_E.csv"
 train_X, test_X, train_label, test_label, T = data_process_SWaT()
 
-# SVM
-# for i in range(1,10,1):
-#     for j in range(1, 10,1):
-#         svm_model = SVC(C=i, gamma=j, kernel='rbf', probability=True)     #C= , gamma =  [11.56584394 10.19786332]
-#         svm_model.fit(train_X,train_label)
-#         # 验证test
-#         y_preds = svm_model.predict(test_X)
-#         report = metrics.classification_report(test_label,y_preds,digits=4)   # 0是正确 1是错误
-#         print(report,i,j)
-
 svm_model = SVC(C=1, gamma=10, kernel='rbf', probability=True)     #C= , gamma =  [11.56584394 10.19786332]
 svm_model.fit(train_X,train_label)
 # 验证testz
